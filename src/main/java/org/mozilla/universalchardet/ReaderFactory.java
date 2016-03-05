221b6702eaea0a34ca9199c01d1fe4e2faacf92c
albfernandez <infjaf@gmail.com>
2016-03-05 19:45:24 +0100

    Utility class to create readers with correct encodings (for files)

---
+++ src/main/java/org/mozilla/universalchardet/ReaderFactory.java
@@ -0,0 +1,27 @@
+package org.mozilla.universalchardet;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+
+public final class ReaderFactory {
+
+	private ReaderFactory() {
+		throw new AssertionError("No instances allowed");
+	}
+	
+	public static Reader createReaderFromFile(File file, Charset defaultCharset) throws IOException {
+		String detectedEncoding = UniversalDetector.detectCharset(file);
+		Charset cs = defaultCharset;
+		if (detectedEncoding != null) {
+			cs = Charset.forName(detectedEncoding);
+		}
+		return new InputStreamReader(new FileInputStream(file), cs);
+	}
+	public static Reader createReaderFromFile(File file) throws IOException {
+		return createReaderFromFile(file, Charset.defaultCharset());
+	}
+}
