33319aabef5bf7ff9e4c8899fab59a332f225add
Alberto Fernandez <infjaf@gmail.com>
2018-12-01 11:27:28 +0100

    Expose a method to check BOM encoding

--- src/main/java/org/mozilla/universalchardet/UniversalDetector.java
+++ src/main/java/org/mozilla/universalchardet/UniversalDetector.java
@@ -157,43 +157,9 @@ public class UniversalDetector
         if (this.start) {
             this.start = false;
             if (length > 3) {
-                int b1 = buf[offset] & 0xFF;
-                int b2 = buf[offset+1] & 0xFF;
-                int b3 = buf[offset+2] & 0xFF;
-                int b4 = buf[offset+3] & 0xFF;
-                
-                switch (b1) {
-                case 0xEF:
-                    if (b2 == 0xBB && b3 == 0xBF) {
-                        this.detectedCharset = Constants.CHARSET_UTF_8;
-                    }
-                    break;
-                case 0xFE:
-                    if (b2 == 0xFF && b3 == 0x00 && b4 == 0x00) {
-                        this.detectedCharset = Constants.CHARSET_X_ISO_10646_UCS_4_3412;
-                    } else if (b2 == 0xFF) {
-                        this.detectedCharset = Constants.CHARSET_UTF_16BE;
-                    }
-                    break;
-                case 0x00:
-                    if (b2 == 0x00 && b3 == 0xFE && b4 == 0xFF) {
-                        this.detectedCharset = Constants.CHARSET_UTF_32BE;
-                    } else if (b2 == 0x00 && b3 == 0xFF && b4 == 0xFE) {
-                        this.detectedCharset = Constants.CHARSET_X_ISO_10646_UCS_4_2143;
-                    }
-                    break;
-                case 0xFF:
-                    if (b2 == 0xFE && b3 == 0x00 && b4 == 0x00) {
-                        this.detectedCharset = Constants.CHARSET_UTF_32LE;
-                    } else if (b2 == 0xFE) {
-                        this.detectedCharset = Constants.CHARSET_UTF_16LE;
-                    }
-                    break;
-                default: 
-                	break;
-                } // swich end
-                
-                if (this.detectedCharset != null) {
+                String detectedBOM = detectCharsetFromBOM(buf, offset);                
+                if (detectedBOM != null) {
+                	this.detectedCharset = detectedBOM;
                     this.done = true;
                     return;
                 }
@@ -253,6 +219,51 @@ public class UniversalDetector
             // do nothing
         }
     }
+    
+    public static String detectCharsetFromBOM(final byte[] buf) {
+    	return detectCharsetFromBOM(buf, 0);
+    }
+    
+	private static String detectCharsetFromBOM(final byte[] buf, int offset) {
+		if (buf.length > (offset + 3)) {
+			int b1 = buf[offset] & 0xFF;
+			int b2 = buf[offset+1] & 0xFF;
+			int b3 = buf[offset+2] & 0xFF;
+			int b4 = buf[offset+3] & 0xFF;
+			
+			switch (b1) {
+			case 0xEF:
+			    if (b2 == 0xBB && b3 == 0xBF) {
+			        return Constants.CHARSET_UTF_8;
+			    }
+			    break;
+			case 0xFE:
+			    if (b2 == 0xFF && b3 == 0x00 && b4 == 0x00) {
+			        return Constants.CHARSET_X_ISO_10646_UCS_4_3412;
+			    } else if (b2 == 0xFF) {
+			        return Constants.CHARSET_UTF_16BE;
+			    }
+			    break;
+			case 0x00:
+			    if (b2 == 0x00 && b3 == 0xFE && b4 == 0xFF) {
+			        return Constants.CHARSET_UTF_32BE;
+			    } else if (b2 == 0x00 && b3 == 0xFF && b4 == 0xFE) {
+			        return Constants.CHARSET_X_ISO_10646_UCS_4_2143;
+			    }
+			    break;
+			case 0xFF:
+			    if (b2 == 0xFE && b3 == 0x00 && b4 == 0x00) {
+			        return Constants.CHARSET_UTF_32LE;
+			    } else if (b2 == 0xFE) {
+			        return Constants.CHARSET_UTF_16LE;
+			    }
+			    break;
+			default: 
+				break;
+			} // swich end
+		}
+		return null;
+	}
     /**
      * Marks end of data reading. Finish calculations.
      */
